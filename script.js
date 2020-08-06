4e50a10841399be63113417412f54f4a26657295
Muhammad Sakib <64504470+Muhammad-Sakib@users.noreply.github.com>
2020-08-06 17:35:12 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function DateAndTime(){
+    var days = new Date();
+    
+    //for time
+    
+    document.getElementById("minutes").innerHTML = days.getMinutes();
+    document.getElementById("seconds").innerHTML = days.getSeconds();
+    var hours = days.getHours();
+    if( hours <=12){
+        document.getElementById("session").innerHTML = "AM";
+    }
+    else{
+        document.getElementById("session").innerHTML = "PM";
+        document.getElementById("hours").innerHTML = days.getHours()-12;
+    }
+    //for date
+    document.getElementById("days").innerHTML = days.getDate();
+    document.getElementById("months").innerHTML = days.getMonth()+1;
+    document.getElementById("years").innerHTML = days.getFullYear();
+    setTimeout('DateAndTime()',500);
+}
